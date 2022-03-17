f3a4450dc18ff83b3b74e783395d15b6a5ec7996
morshedulmunna <morshedulmunna1@gmail.com>
2022-03-17 12:16:06 +0600

    List route added

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const PORT = 8800;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 dotenv.config();
 
@@ -23,6 +26,9 @@ mongoose
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(PORT, () => {
   console.log("Backend Server is running", PORT);
