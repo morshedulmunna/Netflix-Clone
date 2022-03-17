daacbac16cea30abc114529e672f46fbea3ac213
morshedulmunna <morshedulmunna1@gmail.com>
2022-03-17 13:41:53 +0600

    Router navigate

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,34 @@
 import "./App.scss";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
-// import Watch from "./pages/watch/Watch";
-// import Register from "./pages/register/Ragister";
-// import Login from "./pages/login/Login";
+import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Ragister";
+import Login from "./pages/login/Login";
 function App() {
+  const user = true;
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Home type="movies" />} />
-        <Route path="/series" element={<Home type="series" />} />
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate to="/register" />}
+        />
+
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        {user && (
+          <>
+            <Route path="/movies" element={<Home type="movies" />} />
+            <Route path="/series" element={<Home type="series" />} />
+            <Route path="/watch" element={<Watch />} />
+          </>
+        )}
       </Routes>
     </>
   );
